73781a71ce5597794de930fd8b249e67b401cb99
lizaveta <ugodina.elizaveta@mail.ru>
2026-03-04 22:39:04 +0300

    reload

--- app/main.py
+++ app/main.py
@@ -165,4 +165,5 @@ async def test_newsletter(type: str = "morning"):
 if __name__ == "__main__":
     import uvicorn
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run("app.app:app", host="0.0.0.0", port=port, reload=True)
+    # reload=False для продакшена!
+    uvicorn.run("app.app:app", host="0.0.0.0", port=port, reload=False)
